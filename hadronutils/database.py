03da365a21e3135ae4036fefbc1b9c73c14b3c4f
StuartFarmer <email@stuartfarmer.me>
2017-08-14 10:50:21 -0400

    reverted to working_dir old

--- hadronutils/database.py
+++ hadronutils/database.py
@@ -16,29 +16,15 @@ create_contracts = '''
                 instance blob
                 )'''
 select_from = 'SELECT * FROM {table} WHERE {name} {address}'.format
+log = logging.getLogger(__file__)
+connection = sqlite3.connect(DB_FILE)
+cursor = cursor = connection.cursor()
 
-# connection = None
-# cursor = None
-
-def connect_db():
-    log = logging.getLogger(__file__)
-    connection = sqlite3.connect(DB_FILE)
-    cursor = connection.cursor()
-
-# graceful initialization tries to create new tables as a test to see if this is a new DB or not    
+# graceful initialization tries to create new tables as a test to see if this is a new DB or not
 def init_dbs(sqls):
-    log = logging.getLogger(__file__)
-    connection = sqlite3.connect(DB_FILE)
-    cursor = connection.cursor()
     for s in sqls:
-        try:
-            with suppress(sqlite3.OperationalError):
-                cursor.execute(s)
-        except Exception as e:
-            if 'already exists' in str(e):
-                pass
-            else:
-                raise
+        with suppress(sqlite3.OperationalError):
+            cursor.execute(s)
 
 def exec_sql(sql):
     try:
--- hadronutils/settings.py
+++ hadronutils/settings.py
@@ -1,7 +1,14 @@
 import os
 
+import os
 WORKING_DIR = '.'
+cdir = os.path.dirname(__file__)
+WORKING_DIR = os.path.abspath(os.path.join(cdir, WORKING_DIR))
 DB_FILE = os.path.abspath(os.path.join(WORKING_DIR, 'directory.db'))
+try:
+    os.mkdir(WORKING_DIR)
+except OSError as e:
+    pass
 
 def create_working_dir(dir):
 	try:
