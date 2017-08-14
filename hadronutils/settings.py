8b400e89325d356b23486d96bd7136327e082d74
StuartFarmer <email@stuartfarmer.me>
2017-08-14 15:25:58 -0400

    working dir is now the current working directory

--- hadronutils/settings.py
+++ hadronutils/settings.py
@@ -1,9 +1,11 @@
 import os
 
-import os
-WORKING_DIR = '.'
-cdir = os.path.dirname(__file__)
-WORKING_DIR = os.path.abspath(os.path.join(cdir, WORKING_DIR))
+
+# cdir = os.getcwd()
+# WORKING_DIR = os.path.abspath(os.path.join(cdir, WORKING_DIR))
+
+WORKING_DIR = os.getcwd()
+
 DB_FILE = os.path.abspath(os.path.join(WORKING_DIR, 'directory.db'))
 try:
     os.mkdir(WORKING_DIR)
